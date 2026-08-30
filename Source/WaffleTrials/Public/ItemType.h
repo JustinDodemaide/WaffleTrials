3a10b1f196e2ab775cd2f6bd599882160a33b447
Justin <103222511+JustinDodemaide@users.noreply.github.com>
2026-08-29 17:03:45 -0700

    Add item pngs

--- Source/WaffleTrials/Public/ItemType.h
+++ Source/WaffleTrials/Public/ItemType.h
@@ -8,6 +8,10 @@ enum class EItem : uint8
 {
 	None			UMETA(DisplayName = "None"),
 	Bagel			UMETA(DisplayName = "Bagel"),
+	Waffle			UMETA(DisplayName = "None"),
+	Rubbish			UMETA(DisplayName = "Bagel"),
+	Coffee			UMETA(DisplayName = "None"),
+	//Bagel			UMETA(DisplayName = "Bagel"),
 };
 
 USTRUCT(BlueprintType)
